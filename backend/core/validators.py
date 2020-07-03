a56da3f9cbfc06ac1df29883350a996fd44e026f
frandelgado <fdelgado@itba.edu.ar>
2020-07-03 11:26:52 -0300

    Fix image size validation

--- backend/core/validators.py
+++ backend/core/validators.py
@@ -2,7 +2,7 @@ from rest_framework.exceptions import ValidationError
 
 
 def validate_image_size(image):
-    file_size = image.file.size
+    file_size = image.file.getbuffer().nbytes
     limit_mb = 8
     if file_size > limit_mb * 1024 * 1024:
        raise ValidationError("Max size of file is %s MB" % limit_mb)
