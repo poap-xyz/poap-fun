622f170a96865108d28f7721bf0c3e94cf6558eb
frandelgado <fdelgado@itba.edu.ar>
2020-07-08 13:18:59 -0300

    Fix raffle results service

--- backend/core/services/_raffle_results_service.py
+++ backend/core/services/_raffle_results_service.py
@@ -1,10 +1,12 @@
 import math
 import random
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+
+from django.db import transaction
 from web3.auto.infura import w3
 from web3.exceptions import BlockNotFound
 
-from core.models import Raffle, Participant, ResultsTable, BlockData
+from core.models import Raffle, Participant, ResultsTable, BlockData, ResultsTableEntry
 
 
 class RaffleResultsService:
@@ -48,18 +50,23 @@ class RaffleResultsService:
         # are ordered by the address alphabetically
         participants = participants.order_by("address")
 
+        # calculate fixed participants
+        fixed_participants = [result.participant for result in results.all()]
+
         # get the participants that will be participating in the next slicing
-        fixed_participants = results.count()
-        total_participants = participants.count()
-        remaining_participants_amount = total_participants - fixed_participants
-        remaining_participants = participants[:remaining_participants_amount]
+        remaining_participants = []
+        for participant in participants:
+            if participant not in fixed_participants:
+                remaining_participants.append(participant)
+
         return remaining_participants
 
     @staticmethod
     def _save_new_results_table_entries(
             results_table: ResultsTable,
             participants: List[Participant],
-            slicing_percentage: float
+            slicing_percentage: float,
+            block_data: BlockData
     ) -> bool:
         """
         Saves the bottom % of participants to the results table
@@ -75,6 +82,8 @@ class RaffleResultsService:
                 that will be saved to the results table
             results_table:
                 the results table to which the participants are saved
+            block_data:
+                the block data that was used to generate the entries
 
         Returns:
             finished: True if all remaining participants have been set
@@ -82,24 +91,65 @@ class RaffleResultsService:
         """
         amount_of_participants = len(participants)
         amount_of_participants_to_be_saved = math.ceil(amount_of_participants*slicing_percentage)
-        unsaved_participants = participants[:-amount_of_participants_to_be_saved]
-
+        # if we are placing n-1, then the last one is already the winner
+        if amount_of_participants_to_be_saved == amount_of_participants - 1:
+            amount_of_participants_to_be_saved = amount_of_participants
+        unsaved_participants = participants[-amount_of_participants_to_be_saved:]
         # calculate the starting order
         order = amount_of_participants - amount_of_participants_to_be_saved
-        # TODO wrap in atomic, use bulk create and save the block data here
+        entries = []
         for unsaved_participant in unsaved_participants:
-            ResultsTable.objects.create(
+            entry = ResultsTableEntry(
                 results_table=results_table,
                 participant=unsaved_participant,
                 order=order
             )
+            entries.append(entry)
             order += 1
+        # we save the entries and create the block data in the same transaction
+        # so that we do not have an unused corrupted block data in the database
+        # in case something fails between block fetching and result table entry
+        # creation
+        with transaction.atomic():
+            ResultsTableEntry.objects.bulk_create(entries)
+            block_data.save()
+
 
         finalized = amount_of_participants == amount_of_participants_to_be_saved
         if finalized:
             results_table.raffle.finalized = True
             results_table.raffle.save()
         return finalized
+
+    @classmethod
+    def _get_block_data(cls, raffle: Raffle, prev_block: BlockData) -> Optional[BlockData]:
+        """
+        Gets block data from the chain
+
+        Returns:
+            if the block returned from the blockchain equals the prev_block then none
+            else returns a BlockData instance with the current block
+        """
+        try:
+            if prev_block:
+                raw_block_data = w3.eth.getBlock(prev_block.block_number+1)
+            else:
+                raw_block_data = w3.eth.getBlock('latest')
+        except BlockNotFound:
+            return None
+
+        order = prev_block.order + 1 if prev_block else 0
+        nonce = raw_block_data.get("nonce")
+        seed = int.from_bytes(bytes(nonce), "big")
+        block_data = BlockData(
+            raffle=raffle,
+            nonce=nonce,
+            seed=seed,
+            block_number=raw_block_data.get("number"),
+            order=order
+        )
+
+        return block_data
 
     @classmethod
     def generate_next_result_step(cls, raffle: Raffle, slicing_percentage: float) -> bool:
@@ -134,35 +184,5 @@ class RaffleResultsService:
         )
 
         return finished
-
-    @classmethod
-    def _get_block_data(cls, raffle: Raffle, prev_block: BlockData) -> BlockData:
-        """
-        Gets block data from the chain
-
-        Returns:
-            if the block returned from the blockchain equals the prev_block then none
-            else returns a BlockData instance with the current block
-        """
-        try:
-            if prev_block:
-                raw_block_data = w3.eth.getBlock(prev_block.block_number+1)
-            else:
-                raw_block_data = w3.eth.getBlock('latest')
-        except BlockNotFound:
-            return None
-
-        order = prev_block.order + 1 if prev_block else 0
-        nonce = raw_block_data.get("nonce")
-        seed = int.from_bytes(bytes(nonce), "big")
-        block_data = BlockData.objects.create(
-            raffle=raffle,
-            nonce=nonce,
-            seed=seed,
-            block_number=raw_block_data.get("number"),
-            order=order
-        )
-
-        return block_data
 
 
