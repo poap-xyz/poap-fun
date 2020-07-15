4567b46a32e6e39dce2e4d85ae4c24754b7db2fd
frandelgado <fdelgado@itba.edu.ar>
2020-07-15 11:14:11 -0300

    add services

--- backend/core/services/__init__.py
+++ backend/core/services/__init__.py
@@ -1,3 +1,5 @@
-from ._raffle_results_service import RaffleResultsService
+from ._poap_validation_service import PoapValidationService
+
+poap_validation_service = PoapValidationService()
 
 raffle_results_service = RaffleResultsService()
