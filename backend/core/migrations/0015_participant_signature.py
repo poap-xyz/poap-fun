4dc6908a09a03a45a7e0e66d0d1a89d19a9e3d8c
Ramiro Gonzalez <ramiro@xivis.com>
2020-07-18 11:18:40 -0300

    Update 0015_participant_signature.py

--- backend/core/migrations/0015_participant_signature.py
+++ backend/core/migrations/0015_participant_signature.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0011_auto_20200713_1223'),
+        ('core', '0014_auto_20200715_1109'),
     ]
 
     operations = [
