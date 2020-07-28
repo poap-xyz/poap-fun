f44785ed2e6cf24ccf625ff3f6ee495f112c84be
Ramiro Gonzalez <ramiro@xivis.com>
2020-07-28 16:44:51 -0300

    Fix on types

--- frontend/src/lib/helpers/etherscan.ts
+++ frontend/src/lib/helpers/etherscan.ts
@@ -2,7 +2,7 @@ const ETHERSCAN_URL = 'https://etherscan.io';
 const POAP_CONTRACT = process.env.REACT_APP_POAP_CONTRACT;
 
 export const etherscanLinks = {
-  address: (address) => `${ETHERSCAN_URL}/address/${address}`,
-  blocks: (address) => `${ETHERSCAN_URL}/blocks/${address}`,
-  poap: (id) => `${ETHERSCAN_URL}/token/${POAP_CONTRACT}?a=${id}`,
+  address: (address: string) => `${ETHERSCAN_URL}/address/${address}`,
+  blocks: (address: string) => `${ETHERSCAN_URL}/blocks/${address}`,
+  poap: (id: string) => `${ETHERSCAN_URL}/token/${POAP_CONTRACT}?a=${id}`,
 };
