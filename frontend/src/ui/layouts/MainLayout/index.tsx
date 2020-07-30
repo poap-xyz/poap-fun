be3b8348a2d2c58cac3815abbd27d1c0fc7bbac2
Ramiro Gonzalez <ramiro@xivis.com>
2020-07-30 11:47:14 -0300

    Scroll all pages to top

--- frontend/src/ui/layouts/MainLayout/index.tsx
+++ frontend/src/ui/layouts/MainLayout/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 // Components
 import HeaderContent from 'ui/components/Header';
@@ -10,16 +11,24 @@ type MainLayoutProps = {
   padded?: boolean;
 };
 
-const MainLayout: FC<MainLayoutProps> = ({ children, padded = true }) => (
-  <Layout padded={padded}>
-    <Header>
-      <HeaderContent />
-    </Header>
-    <Content>{children}</Content>
-    <Footer>
-      <FooterContent />
-    </Footer>
-  </Layout>
-);
+const MainLayout: FC<MainLayoutProps> = ({ children, padded = true }) => {
+  const location = useLocation();
+  // Scroll to top if path changes
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <Layout padded={padded}>
+      <Header>
+        <HeaderContent />
+      </Header>
+      <Content>{children}</Content>
+      <Footer>
+        <FooterContent />
+      </Footer>
+    </Layout>
+  );
+};
 
 export default MainLayout;
