3ab8aea918d28613c6edbe0d8866107965db7583
Nahuel <naha90@gmail.com>
2020-09-03 15:05:33 -0300

    Bcc on new raffle email (#37)
    * https://xivis.atlassian.net/browse/POAP-531
    
    * Add support for multiple BCCs
    
    Co-authored-by: Ramiro Gonzalez <ramiro@xivis.com>

--- backend/core/emails.py
+++ backend/core/emails.py
@@ -1,8 +1,11 @@
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
 from .models import EmailConfiguration
 from .services import EmailService
 
 
-def send_raffle_created_email(raffle):
+def send_raffle_created_email(raffle, token):
     service = EmailService()
 
     service.create_message(
@@ -13,8 +16,16 @@ def send_raffle_created_email(raffle):
     service.set_template(config.raffle_created_template)
     data = {
         'raffle_id': raffle.id,
-        'raffle_token': raffle._token,
+        'raffle_token': token,
         'raffle_draw_datetime': f'{raffle.draw_datetime.strftime("%d-%b-%Y %H:%M")} UTC' if raffle.draw_datetime else 'TBD'
     }
     service.set_data(data)
+    if config.new_raffle_bcc_email and len(config.new_raffle_bcc_email.split(',')) > 0:
+        for bcc in config.new_raffle_bcc_email.split(','):
+            try:
+                validate_email(bcc)
+                service.set_bcc(bcc)
+            except ValidationError as e:
+                print("bad email, details:", e)
+
     service.send_email()
